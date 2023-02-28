ca170f4e802035ed676f32145b8a71295870c02d
ElBasillio <113295120+ElBasillio@users.noreply.github.com>
2023-02-28 02:03:01 +0200

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,20 +1,20 @@
 function showText() {
-    const x = document.getElementById("text-input");
-    const text = x.value;
+  const x = document.getElementById("text-input");
+  const text = x.value;
 
-    const textNode = document.getElementById("text-output")
+  const textNode = document.getElementById("text-output")
 
-    const words = text.split(" "); 
+  const words = text.split(" "); 
 
-    if(text) {
-      document.getElementById("input-area").remove()
-      textNode.style.height = '100vh';
+  if(text) {
+    document.getElementById("input-area").remove()
+    textNode.style.height = '100vh';
 
-      const intervalId = setInterval(function() { 
-        if (!words.length) {
-          return clearInterval(intervalId); 
-        }
-        textNode.innerText = " " + words.shift();
-      }, 100);
-    }
+    const intervalId = setInterval(function() { 
+      if (!words.length) {
+        return clearInterval(intervalId); 
+      }
+      textNode.innerText = " " + words.shift();
+    }, 500);
   }
+}
