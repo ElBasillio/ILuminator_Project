9be6950a88f1535729eea5f221f72d8f71830458
OlehPoloviy <olehtratata@gmail.com>
2023-03-14 21:30:05 +0200

    working buttons

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,39 +1,44 @@
 let varSpeed = 500;
 
-
 function showText() {
   const x = document.getElementById("text-input");
   const text = x.value;
 
-  const textNode = document.getElementById("text-output")
+  const textNode = document.getElementById("text-output");
 
-  const words = text.split(" "); 
+  const words = text.split(" ");
 
-  if(text) {
-    document.getElementById("input-area").remove()
-    textNode.style.height = '100vh';
+  if (text) {
+    document.getElementById("input-area").remove();
+    textNode.style.height = "100vh";
 
-    const intervalId = setInterval(function() { 
+    const intervalId = setInterval(function () {
       if (!words.length) {
-        return clearInterval(intervalId); 
+        return clearInterval(intervalId);
       }
       textNode.innerText = " " + words.shift();
-    }, varSpeed);
+    }, 150000 / varSpeed);
   }
 }
 
-function speedIncrease(){
-  return varSpeed += 50;
+function speedIncrease() {
+  varSpeed += 50;
+  document.getElementById("hereSpeed").innerHTML = varSpeed;
 }
 
-function speedDecrease(){
-  return varSpeed -= 50;
+function speedDecrease() {
+  varSpeed -= 50;
+  document.getElementById("hereSpeed").innerHTML = varSpeed;
 }
+
+function speedSet() {}
 
 //Сюди вписувати кількість слів за хвилину
-function speedSet(){
-
+function speedSet() {
+  varSpeed =
+    60000 / parseInt(document.getElementById("words-per-minute").value);
+  document.getElementById("hereSpeed").innerHTML = varSpeed;
 }
 //Зробити грамотний вивід швидкості слів
 //Це разовий вивід
-document.getElementById("hereSpeed").innerHTML=varSpeed;
+document.getElementById("hereSpeed").innerHTML = varSpeed;
