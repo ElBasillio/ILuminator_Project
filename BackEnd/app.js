47f09c95940bdc63701de8d61c3f9c94fdb2ceca
OlehPoloviy <olehtratata@gmail.com>
2023-04-24 21:09:07 +0300

    pause button

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -60,6 +60,64 @@ function showText() {
     }, intervalTime);
   }
 }
+let pause = false;
+
+function showTextWithPause() {
+  const x = document.getElementById("text-input");
+  const text = x.value;
+  const textNode = document.getElementById("text-output");
+  const words = text.split(" ");
+  const targetSpeed = 200; // set your desired speed here
+  let currentSpeed = varSpeed; // default speed
+  const intervalTime = 150000 / currentSpeed;
+  const pauseButton = document.querySelector(".pauseControl");
+
+  pauseButton.style.display = "none"; // hide the pause button by default
+
+  if (text) {
+    document.getElementById("input-area").remove();
+    textNode.style.height = "100vh";
+    pauseButton.style.display = "flex"; // show the pause button
+    pauseButton.style.justifyContent = "center"; // center the button horizontally
+    pauseButton.style.bottom = "0"; // align the button to the bottom
+    pauseButton.style.position = "absolute"; // position the button absolutely
+    
+
+    const intervalId = setInterval(function () {
+      if (pause) return; // check if pause is enabled
+
+      if (!words.length) {
+        pauseButton.style.display = "none"; // hide the pause button
+        return clearInterval(intervalId);
+      }
+
+      textNode.innerText = " " + words.shift();
+
+      // Increase speed until target speed is reached
+      if (currentSpeed < targetSpeed) {
+        const speedIncreaseRate = (targetSpeed - currentSpeed) * 0.1 / targetSpeed; // modify speed increase rate here
+        currentSpeed += currentSpeed * speedIncreaseRate;
+        clearInterval(intervalId);
+        setInterval(function () {
+          if (pause) return; // check if pause is enabled
+
+          if (!words.length) {
+            pauseButton.style.display = "none"; // hide the pause button
+            return clearInterval(intervalId);
+          }
+
+          textNode.innerText = " " + words.shift();
+        }, 150000 / currentSpeed);
+      }
+    }, intervalTime);
+  }
+}
+
+
+function togglePause() {
+  pause = !pause;
+}
+
 
 function speedIncrease() {
   varSpeed += 10;
