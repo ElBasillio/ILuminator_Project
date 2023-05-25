3e2dd0948d36b027cd0d5970c8fb82c3e2d8a709
PavlykOstap <126407846+PavlykOstap@users.noreply.github.com>
2023-05-25 18:41:01 +0300

    Пофіксив баг з виводом тексту

--- BackEnd/PDF.js
+++ BackEnd/PDF.js
@@ -1,18 +1,18 @@
-// створюємо функцію для читання тексту з PDF
 async function readPdfFile(file) {
-  // завантажуємо PDF-файл
   const pdf = await pdfjsLib.getDocument({ url: URL.createObjectURL(file) }).promise;
-
-  // зчитуємо кількість сторінок у PDF
   const numPages = pdf.numPages;
-  // проходимось по всіх сторінках і зчитуємо текст
   let fullText = "";
+
   for (let i = 1; i <= numPages; i++) {
     const page = await pdf.getPage(i);
     const content = await page.getTextContent();
-    const text = content.items.map((item) => item.str).join("");
-    fullText += text + "\n";
+    const textItems = content.items.map((item) => item.str);
+
+    // Об'єднати рядки в один абзац з використанням переносу рядка
+    const paragraph = textItems.join("\n");
+    fullText += paragraph + "\n\n"; // Додати два переноси рядка після кожного абзацу
   }
+
   const textContainer = document.getElementById("text-container");
   textContainer.textContent = fullText;
 }
@@ -26,7 +26,7 @@ readBtn.addEventListener("click", async () => {
     try {
       await readPdfFile(file);
     } catch (error) {
-      console.error(error); // виводимо текст у консоль
+      console.error(error);
     }
   }
 });
